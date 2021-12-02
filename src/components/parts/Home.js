964014d8ab5d43a650ec6f98b51e5bfe1aafc280
graham24 <gdholland01@gmail.com>
2021-12-02 14:32:33 -0700

    remove transitioons stuff

--- src/components/parts/Home.js
+++ src/components/parts/Home.js
@@ -1,21 +1,5 @@
 import React from "react";
 
-import { Transition } from 'react-transition-group';
-
-const duration = 300;
-
-const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0,
-}
-
-const transitionStyles = {
-    entering: { opacity: 1 },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 },
-};
-
 
 const Home = () => {
     return (
