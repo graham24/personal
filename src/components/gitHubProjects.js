fefd926b6951b9d94478c7f7e70a3d4ee13bd192
graham24 <gdholland01@gmail.com>
2021-12-08 15:25:44 -0800

    fix clicked item

--- src/components/gitHubProjects.js
+++ src/components/gitHubProjects.js
@@ -6,21 +6,32 @@ import { fetchProjectREADME } from "../actions";
 import ProjectDetail from "./gitHubProjectDetail";
 
 class projectList extends React.Component {
+    state = {
+        activeItem: null,
+    }
+
     componentDidMount() {
         this.props.fetchProjects();
     }
     componentDidUpdate() {
         var projects = this.props.projects;
-        this.props.fetchProjectREADME(projects[0].name, projects[0].default_branch, projects[0].id)
+       // this.props.fetchProjectREADME(projects[0].name, projects[0].default_branch, projects[0].id)
+    }
+    selectProject(project) {
+        this.setState({ activeItem: project.id });
+        this.props.fetchProjectREADME(project.name, project.default_branch, project.id);
     }
     renderList() {
+        const { activeItem } = this.state;
+
         return this.props.projects.map((project) => {
             return (
-                <div key={project.id} onClick={() => this.props.fetchProjectREADME(project.name, project.default_branch, project.id)} className="hover:bg-gray-50 hover:bg-opacity-5 rounded-md hover:cursor-pointer">
+                <div key={project.id} onClick={() => this.selectProject(project)} className={"mb-4 hover:bg-gray-50 hover:bg-opacity-5 rounded-md hover:cursor-pointer" + (project.id === activeItem ? ' bg-gray-50 bg-opacity-5' : '')}>
                     <div className="md:grid md:grid-cols-third p-4">
                         <div className="single-project cursor-pointer pt-4 md:p-4">
                             <div className="project-name"><h3>{project.name}</h3></div>
                             <div className="description">{project.description}</div>
+                            <div className=""><a href={project.html_url} target="_blank">{project.html_url}</a></div>
                         </div>
                         <div className="pt-4">
                             <ProjectDetail projectid={project.id} />
