60de83787adb509c67d65ee4af70fbf3047c3aaf
graham24 <gdholland01@gmail.com>
2021-12-10 15:49:56 -0800

    remove btc price

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import BTCPrice from './btcPrice';
+// import BTCPrice from './btcPrice';
 
 const Footer = () => {
     return (
         <footer className="md:col-span-2 lg:col-span-2 flex items-end p-4">
-            <BTCPrice />
+            
         </footer>
     )
 }
