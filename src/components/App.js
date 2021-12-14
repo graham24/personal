8b98176551d8357baa2ace4026dec7410ee2e61b
graham24 <gdholland01@gmail.com>
2021-12-14 13:15:27 -0800

    more lazzy loading

--- src/components/App.js
+++ src/components/App.js
@@ -17,20 +17,20 @@ const App = () => {
     return (
         <div id="main-content" className="dark:text-white bg-white dark:bg-gray-800 grid grid-cols-1 md:grid-cols-medium lg:grid-cols-large xl:grid-cols-xl min-h-screen max-w-screen">
             <BrowserRouter>
-                <Header />
-                <div className="h-auto overflow-auto max-h-auto flex md:pt-40">
-                    <Suspense fallback={<div>Loading...</div>}>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Header />
+                    <div className="h-auto overflow-auto max-h-auto flex md:pt-40">
                         <Route path="/" exact component={About} />
                         <Route path="/experience" exact component={jobList} />
                         <Route path="/personal-projects" exact component={personalProjectList} />
                         <Route path="/work-projects" exact component={workProjectList} />
-                    </Suspense>
-                </div>
+                    </div>
+                    <Footer />
+                </Suspense>
                 <div>
                     <RouteChangeTracker />
                 </div>
             </BrowserRouter>
-            <Footer />
         </div>
     )
 };
