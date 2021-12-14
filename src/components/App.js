795cf54fb066ad6e7d6155a77d5e6a2f2b67d69c
graham24 <gdholland01@gmail.com>
2021-12-14 12:57:40 -0800

    add lazy loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import jobList from './jobList';
-// import jobDetail from './jobDetail';
-import personalProjectList from './gitHubPersonalProjects';
-import workProjectList from './gitHubWorkProjects';
-// import projectDetail from './gitHubProjectDetail';
 
 import RouteChangeTracker from './RouteChangeTracker'
-
-
-// import Home from './parts/Home';
-import About from './parts/About';
+import ReactGA from 'react-ga';
 
 import Header from './Header';
 import Footer from './Footer';
 
-import ReactGA from 'react-ga';
+const jobList = lazy(() => import('./jobList'));
+const personalProjectList = lazy(() => import('./gitHubPersonalProjects'));
+const workProjectList = lazy(() => import('./gitHubWorkProjects'));
+const About = lazy(() => import('./parts/About'));
+
+
+
+
 ReactGA.initialize('UA-215325150-1');
 
 const App = () => {
     return (
         <div id="main-content" className="dark:text-white bg-white dark:bg-gray-800 grid grid-cols-1 md:grid-cols-medium lg:grid-cols-large xl:grid-cols-xl min-h-screen max-w-screen">
             <BrowserRouter>
-
                 <Header />
                 <div className="h-auto overflow-auto max-h-auto flex md:pt-40">
-                    <Route path="/" exact component={About} />
-                    <Route path="/experience" exact component={jobList} />
-                    <Route path="/personal-projects" exact component={personalProjectList} />
-                    <Route path="/work-projects" exact component={workProjectList} />
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Route path="/" exact component={About} />
+                        <Route path="/experience" exact component={jobList} />
+                        <Route path="/personal-projects" exact component={personalProjectList} />
+                        <Route path="/work-projects" exact component={workProjectList} />
+                    </Suspense>
                 </div>
                 <div>
                     <RouteChangeTracker />
