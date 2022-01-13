c30cc79a992eaefa46dbfad00e3b9873f2b93ccc
graham24 <gdholland01@gmail.com>
2022-01-13 11:56:27 -0800

    fix lazy loading header footer

--- src/components/App.js
+++ src/components/App.js
@@ -17,16 +17,16 @@ const App = () => {
     return (
         <div id="main-content" className="dark:text-white bg-white dark:bg-gray-800 grid grid-cols-1 md:grid-cols-medium lg:grid-cols-large xl:grid-cols-xl min-h-screen max-w-screen">
             <BrowserRouter>
+                <Header /> 
                 <Suspense fallback={<div>Loading...</div>}>
-                    <Header />
                     <div className="h-auto overflow-auto max-h-auto flex md:pt-40">
                         <Route path="/" exact component={About} />
                         <Route path="/experience" exact component={jobList} />
                         <Route path="/personal-projects" exact component={personalProjectList} />
                         <Route path="/work-projects" exact component={workProjectList} />
                     </div>
-                    <Footer />
                 </Suspense>
+                <Footer />
                 <div>
                     <RouteChangeTracker />
                 </div>
